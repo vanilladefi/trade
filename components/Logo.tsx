98095c1bf0eb542a30f34b2bb59259e75cc7356d
Jani Anttonen <jantto@utu.fi>
2020-11-25 15:43:07 +0200

    Remove unnecessary eager loading

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 
 const Logo = () => (
-  <Image src="/images/logo-vanilla.svg" alt='Vanilla logo' loading='eager' width='120px' height='28px' layout="fixed" />
+  <Image src="/images/logo-vanilla.svg" alt='Vanilla logo' width='120px' height='28px' layout="fixed" />
 )
 
 export default Logo
