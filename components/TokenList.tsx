6fb7f8178d4a51266a77abcde28e7b993a0c65d5
Jani Anttonen <jantto@utu.fi>
2020-12-10 16:54:24 +0200

    Style tables in line with designs

--- components/TokenList.tsx
+++ components/TokenList.tsx
@@ -1,65 +1,23 @@
 import React from 'react'
 import { useTable } from 'react-table'
 
-type Token = {
-  imageUrl: string
-  name: string
-  ticker: string
-  price: string
-  marketCap: string
-  liquidity: string
-  priceChange: number
-}
-
 type Props = {
-  tokens?: Array<Token>
+  data: {
+    imageUrl: string
+    name: string
+    ticker: string
+    price: string
+    marketCap: string
+    liquidity: string
+    priceChange: number
+  }[]
+  columns: {
+    Header: string
+    accessor: string
+  }[]
 }
 
-const mockup: Array<Token> = [
-  {
-    imageUrl: '/images/uniswap.png',
-    name: 'Uniswap',
-    ticker: 'UNI',
-    price: '$447.63',
-    marketCap: '$1,000,000,000',
-    liquidity: '$26,364,263',
-    priceChange: -1.25,
-  },
-]
-
-const TokenList = ({ tokens = mockup }: Props): JSX.Element => {
-  const data = React.useMemo(
-    () => [
-      {
-        col1: 'Hello',
-        col2: 'World',
-      },
-      {
-        col1: 'react-table',
-        col2: 'rocks',
-      },
-      {
-        col1: 'whatever',
-        col2: 'you want',
-      },
-    ],
-    []
-  )
-
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Column 1',
-        accessor: 'col1', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Column 2',
-        accessor: 'col2',
-      },
-    ],
-    []
-  )
-
+const TokenList = ({ data, columns }: Props): JSX.Element => {
   const {
     getTableProps,
     getTableBodyProps,
@@ -91,6 +49,43 @@ const TokenList = ({ tokens = mockup }: Props): JSX.Element => {
           )
         })}
       </tbody>
+      <style jsx>{`
+        table {
+          border-collapse: separate;
+          table-layout: auto;
+          width: calc(100% + 34px);
+          margin-left: -17px;
+          margin-right: -17px;
+          font-family: var(--bodyfont);
+          font-size: var(--bodysize);
+          border-spacing: 0 13px;
+          margin-top: -13px;
+        }
+        td,
+        th {
+          text-align: left;
+          padding: 14px 17px;
+          border-spacing: 13px 0;
+        }
+        th {
+          font-weight: var(--theadweight);
+          font-size: var(--smallsize);
+          text-transform: uppercase;
+          color: rgba(#2c1929, 0.6);
+          padding: 0px 17px;
+        }
+        tr td:first-of-type {
+          border-top-left-radius: 9999px;
+          border-bottom-left-radius: 9999px;
+        }
+        tr td:last-of-type {
+          border-top-right-radius: 9999px;
+          border-bottom-right-radius: 9999px;
+        }
+        td {
+          background: var(--beige);
+        }
+      `}</style>
     </table>
   )
 }
--- pages/trade/index.tsx
+++ pages/trade/index.tsx
@@ -1,12 +1,29 @@
 import Link from 'next/link'
+import React, { useState } from 'react'
 import Gradient from '../../components/backgrounds/gradient'
-import BoxSection, { Color } from '../../components/BoxSection'
 import { Column, Row, Width } from '../../components/grid/Flex'
 import Button from '../../components/input/Button'
 import Layout from '../../components/Layout'
+import TokenList from '../../components/TokenList'
 import HugeMonospace from '../../components/typography/HugeMonospace'
 import { Title } from '../../components/typography/Titles'
 import Wrapper from '../../components/Wrapper'
+
+type Props = {
+  data?: {
+    imageUrl: string
+    name: string
+    ticker: string
+    price: string
+    marketCap: string
+    liquidity: string
+    priceChange: number
+  }[]
+  columns?: {
+    Header: string
+    accessor: string
+  }[]
+}
 
 export const HeaderContent: JSX.Element = (
   <>
@@ -29,23 +46,95 @@ export const HeaderContent: JSX.Element = (
   </>
 )
 
-export const BodyContent = (): JSX.Element => {
+export const BodyContent = (props: Props): JSX.Element => {
   return (
     <Wrapper>
-      <BoxSection color={Color.WHITE}>
-        <h1>List of tokens comes here</h1>
-        <Link href='/trade/ebin'>Open latest trade</Link>
-      </BoxSection>
+      <Row>
+        <Column width={Width.TWELVE}>
+          <h1>List of tokens comes here</h1>
+          <Link href='/trade/ebin'>Open latest trade</Link>
+          <TokenList {...props} />
+        </Column>
+      </Row>
     </Wrapper>
   )
 }
 
-const TradePage = (): JSX.Element => (
-  <>
-    <Layout title='Trade | Vanilla' hero={HeaderContent}>
-      <BodyContent />
-    </Layout>
-  </>
-)
+const TradePage = (): JSX.Element => {
+  const data = React.useMemo(
+    () => [
+      {
+        imageUrl: '/images/uniswap.png',
+        name: 'Uniswap',
+        ticker: 'UNI',
+        price: '$447.63',
+        marketCap: '$1,000,000,000',
+        liquidity: '$26,364,263',
+        priceChange: -1.25,
+      },
+      {
+        imageUrl: '/images/uniswap.png',
+        name: 'Uniswap',
+        ticker: 'UNI',
+        price: '$447.63',
+        marketCap: '$1,000,000,000',
+        liquidity: '$26,364,263',
+        priceChange: -1.25,
+      },
+      {
+        imageUrl: '/images/uniswap.png',
+        name: 'Uniswap',
+        ticker: 'UNI',
+        price: '$447.63',
+        marketCap: '$1,000,000,000',
+        liquidity: '$26,364,263',
+        priceChange: -1.25,
+      },
+    ],
+    []
+  )
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Logo',
+        accessor: 'imageUrl',
+      },
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Ticker',
+        accessor: 'ticker',
+      },
+      {
+        Header: 'Price',
+        accessor: 'price',
+      },
+      {
+        Header: 'Market Cap',
+        accessor: 'marketCap',
+      },
+      {
+        Header: 'Liquidity',
+        accessor: 'liquidity',
+      },
+      {
+        Header: 'Change',
+        accessor: 'priceChange',
+      },
+    ],
+    []
+  )
+  const [modalOpen, setModalOpen] = useState(false)
+  return (
+    <>
+      <Layout title='Trade | Vanilla' hero={HeaderContent}>
+        <Modal></Modal>
+        <BodyContent data={data} columns={columns} />
+      </Layout>
+    </>
+  )
+}
 
 export default TradePage
