a4ed680d103e0efba44a76d05e5c8d5633fb3f27
Jani Anttonen <jantto@utu.fi>
2020-12-15 17:28:07 +0200

    Start implementing the wallet with ethers + web3 providers

--- components/state/Wallet.ts
+++ components/state/Wallet.ts
@@ -1,3 +1,19 @@
+import ethers from 'ethers'
 import React from 'react'
 
-export const WalletContext = React.createContext({})
+const providers = {
+  'Injected Web3 runtime': {
+    getWeb3: () => {
+      if (!window.web3) throw new Error('No web3 instance found in runtime')
+      const provider = new ethers.providers.Web3Provider(
+        window.web3.currentProvider
+      )
+      const signer = provider.getSigner()
+      return provider, signer
+    },
+  },
+}
+
+const WalletContext = React.createContext({ modalOpen: false })
+
+export { WalletContext, providers }
