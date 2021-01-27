7bdd33dd21f890429998e7fd6c905f28f672b42d
JaniAnttonen <jani.anttonen@protonmail.ch>
2021-01-27 15:25:38 +0200

    Load FAQ from .mdx

---
+++ components/typography/ResponsiveImage.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import { useState } from 'react'
+
+type Props = {
+  src: string
+}
+
+const ResponsiveImage = ({ src }: Props): JSX.Element => {
+  const [dimensions, setDimensions] = useState({ height: 0, width: 0 })
+  return (
+    <>
+      <div>
+        <Image
+          src={src}
+          layout='responsive'
+          objectFit='scale-down'
+          width={dimensions.width !== 0 ? dimensions.width : '1'}
+          height={`${dimensions.height}px`}
+          onLoad={(element) =>
+            setDimensions({
+              height: element.currentTarget.naturalHeight,
+              width: element.currentTarget.naturalWidth,
+            })
+          }
+        />
+      </div>
+      <style jsx>{`
+        div {
+          display: 'flex';
+          position: relative;
+          min-height: 1px;
+          min-width: 1px;
+          height: fit-content;
+          max-width: 100%;
+          flex-shrink: 1;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export default ResponsiveImage
