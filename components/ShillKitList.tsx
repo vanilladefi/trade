8ea7ce577f473d0042777a02a0a25c4bae19a396
Joni Juup <jonijuup@gmail.com>
2021-02-26 16:39:41 +0200

    shill kit on the frontpage, need to filter it a bit and adjust styles

---
+++ components/ShillKitList.tsx
@@ -0,0 +1,79 @@
+const ShillKitList = [
+  {
+    description: '#ProfitMining, good',
+    url: '/shill/vnl--blossomman.gif',
+    type: 'animated',
+  },
+  {
+    description: 'Just chilling, and mining',
+    url: '/shill/vnl--chillingandmining.jpg',
+    type: 'image',
+  },
+  {
+    description: 'Cup of profit',
+    url: '/shill/vnl--cupofprofit.jpg',
+    type: 'image',
+  },
+  {
+    description: 'Do you even #ProfitMine, bro?',
+    url: '/shill/vnl--doyoueven.jpg',
+    type: 'image',
+  },
+  {
+    description: 'Lit',
+    url: '/shill/vnl--fire.gif',
+    type: 'animated',
+  },
+  {
+    description: 'Splash',
+    url: '/shill/vnl--ink.gif',
+    type: 'animated',
+  },
+  {
+    description: 'Get inspired',
+    url: '/shill/vnl--inspirational.jpg',
+    type: 'image',
+  },
+  {
+    description: 'Live Laugh #ProfitMine',
+    url: '/shill/vnl--livelaughprofitmine.jpg',
+    type: 'image',
+  },
+  {
+    description: 'New cool kid on the scene',
+    url: '/shill/vnl--miner.gif',
+    type: 'animated',
+  },
+  {
+    description: 'Motivational',
+    url: '/shill/vnl--motivational.jpg',
+    type: 'image',
+  },
+  {
+    description: 'Those profitmining feels',
+    url: '/shill/vnl--profitminingfeels.gif',
+    type: 'animated',
+  },
+  {
+    description: 'Yes, Thumbs up for ProfitMining',
+    url: '/shill/vnl--thumbsup.gif',
+    type: 'animated',
+  },
+  {
+    description: 'Ride the wave',
+    url: '/shill/vnl--wave.jpg',
+    type: 'image',
+  },
+  {
+    description: 'Working hard',
+    url: '/shill/vnl--working.gif',
+    type: 'animated',
+  },
+  {
+    description: 'Yo dawg',
+    url: '/shill/vnl--yodawg.jpg',
+    type: 'image',
+  },
+]
+
+export default ShillKitList
--- pages/shill-kit.tsx
+++ pages/shill-kit.tsx
@@ -4,84 +4,7 @@ import Layout from '../components/Layout'
 import Wrapper from '../components/Wrapper'
 import { Title } from '../components/typography/Titles'
 import { BreakPoint } from '../components/GlobalStyles/Breakpoints'
-
-const shills = [
-  {
-    description: '#ProfitMining, good',
-    url: '/shill/vnl--blossomman.gif',
-    type: 'animated',
-  },
-  {
-    description: 'Just chilling, and mining',
-    url: '/shill/vnl--chillingandmining.jpg',
-    type: 'image',
-  },
-  {
-    description: 'Cup of profit',
-    url: '/shill/vnl--cupofprofit.jpg',
-    type: 'image',
-  },
-  {
-    description: 'Do you even #ProfitMine, bro?',
-    url: '/shill/vnl--doyoueven.jpg',
-    type: 'image',
-  },
-  {
-    description: 'Lit',
-    url: '/shill/vnl--fire.gif',
-    type: 'animated',
-  },
-  {
-    description: 'Splash',
-    url: '/shill/vnl--ink.gif',
-    type: 'animated',
-  },
-  {
-    description: 'Get inspired',
-    url: '/shill/vnl--inspirational.jpg',
-    type: 'image',
-  },
-  {
-    description: 'Live Laugh #ProfitMine',
-    url: '/shill/vnl--livelaughprofitmine.jpg',
-    type: 'image',
-  },
-  {
-    description: 'New cool kid on the scene',
-    url: '/shill/vnl--miner.gif',
-    type: 'animated',
-  },
-  {
-    description: 'Motivational',
-    url: '/shill/vnl--motivational.jpg',
-    type: 'image',
-  },
-  {
-    description: 'Those profitmining feels',
-    url: '/shill/vnl--profitminingfeels.gif',
-    type: 'animated',
-  },
-  {
-    description: 'Yes, Thumbs up for ProfitMining',
-    url: '/shill/vnl--thumbsup.gif',
-    type: 'animated',
-  },
-  {
-    description: 'Ride the wave',
-    url: '/shill/vnl--wave.jpg',
-    type: 'image',
-  },
-  {
-    description: 'Working hard',
-    url: '/shill/vnl--working.gif',
-    type: 'animated',
-  },
-  {
-    description: 'Yo dawg',
-    url: '/shill/vnl--yodawg.jpg',
-    type: 'image',
-  },
-]
+import ShillKitList from '../components/ShillKitList'
 
 const ShillKit = (): JSX.Element => {
   return (
@@ -90,14 +13,8 @@ const ShillKit = (): JSX.Element => {
         <div style={{ padding: '1rem' }}>
           <Title>Shill kit</Title>
         </div>
-        <div
-          className='galleryContainer'
-          style={{
-            columnGap: '1rem',
-            paddingBottom: 'rem',
-          }}
-        >
-          {shills.map((shill) => (
+        <div className='galleryContainer'>
+          {ShillKitList.map((shill) => (
             <div
               style={{ width: '100%', maxWidth: '400px', padding: '0 0 .5rem' }}
             >
