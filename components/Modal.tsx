acdd2a7b38b1fe4d30ffeae39bb5b8a5926f6a1b
Jani Anttonen <jantto@utu.fi>
2021-03-19 14:21:44 +0200

    Remove onClick e.preventDefault from modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -35,7 +35,6 @@ const Modal = ({
           <div
             className='modal'
             onClick={(e) => {
-              e.preventDefault()
               e.stopPropagation()
             }}
           >
