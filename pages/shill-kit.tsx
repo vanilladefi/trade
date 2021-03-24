f0ae60655aa4e7f8069f2ccf40b49378569add2b
Joni Juup <jonijuup@gmail.com>
2021-03-24 10:38:21 +0200

    fixed lint error

--- pages/shill-kit.tsx
+++ pages/shill-kit.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import Wrapper from '../components/Wrapper'
-import { Title } from '../components/typography/Titles'
 import ShillKitList from '../components/ShillKitList'
 
 const ShillKit = (): JSX.Element => {
