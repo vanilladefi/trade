776a629bce3fc586496431685c7d5c836de4a65b
Jani Anttonen <jantto@utu.fi>
2021-03-26 10:49:28 +0200

    Add a separate RPC URL for SSR

--- utils/config.ts
+++ utils/config.ts
@@ -10,6 +10,10 @@ export const vanillaRouterAddress: string =
   '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 
 export const rpcUrl: string =
-  process.env.NEXT_PUBLIC_RPC_URL || 'http://localhost:8545'
+  (process.env.SSR_RPC_URL &&
+    process.env.SSR_RPC_URL !== '' &&
+    process.env.SSR_RPC_URL) ||
+  process.env.NEXT_PUBLIC_RPC_URL ||
+  'http://localhost:8545'
 
 export const defaultProvider = new providers.JsonRpcProvider(rpcUrl, chainId)
