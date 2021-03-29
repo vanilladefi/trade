e87521ed653a5263d70b8a94a88e997b806d33d6
Jani Anttonen <jantto@utu.fi>
2021-03-29 19:53:41 +0300

    Remove ETH from totals

--- hooks/useTotalOwned.ts
+++ hooks/useTotalOwned.ts
@@ -1,18 +1,11 @@
-import { formatUnits } from 'ethers/lib/utils'
 import { useMemo } from 'react'
 import { useRecoilValue } from 'recoil'
-import { currentETHPrice } from 'state/meta'
 import { userTokensState } from 'state/tokens'
-import { useWallet } from 'use-wallet'
 
 function useTotalOwned(): { USD: number; ETH: number } {
   const userTokens = useRecoilValue(userTokensState)
-  const ethPrice = useRecoilValue(currentETHPrice)
-  const { balance } = useWallet()
 
   return useMemo(() => {
-    const parsedETHBalance = parseFloat(formatUnits(balance, 18))
-
     const values =
       userTokens &&
       userTokens.filter((token) => !!token.value).map((token) => token.value)
@@ -44,13 +37,13 @@ function useTotalOwned(): { USD: number; ETH: number } {
 
     if (tokenSum) {
       return {
-        USD: tokenSum + parsedETHBalance * ethPrice,
-        ETH: totalTokenValueInEth + parsedETHBalance,
+        USD: tokenSum,
+        ETH: totalTokenValueInEth,
       }
     } else {
       return { USD: 0, ETH: 0 }
     }
-  }, [balance, ethPrice, userTokens])
+  }, [userTokens])
 }
 
 export default useTotalOwned
