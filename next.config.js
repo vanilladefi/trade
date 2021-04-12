c290f73f4c6d57c50cdc35ba24704196a255fb17
Jani Anttonen <jantto@utu.fi>
2021-04-12 16:20:34 +0300

    Update CSP with connect-src values

--- next.config.js
+++ next.config.js
@@ -31,7 +31,8 @@ module.exports = withMDX({
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' 'unsafe-inline'",
+            value:
+              "default-src 'self'; img-src 'self' *raw.githubusercontent.com https://ipfs.io https://assets.coingecko.com https://vanilladefi.com *equilibriumco.vercel.app; script-src 'self' 'unsafe-inline' https://vanilladefi.com https://plausible.io *equilibriumco.vercel.app; style-src 'self' 'unsafe-inline'; connect-src 'self' https://api.thegraph.com wss://api.thegraph.com https://eth-mainnet.alchemyapi.io wss://eth-mainnet.alchemyapi.io",
           },
           {
             key: 'X-XSS-Protection',
@@ -44,6 +45,10 @@ module.exports = withMDX({
           {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: "camera('none'); microphone('none'); geolocation('none')",
           },
         ],
       },
