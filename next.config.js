8923636f1dba8b0f082aa7fe5bfea069bcb34e18
Jani Anttonen <jantto@utu.fi>
2021-04-12 14:31:44 +0300

    Add some security headers

--- next.config.js
+++ next.config.js
@@ -29,6 +29,19 @@ module.exports = withMDX({
             key: 'X-Frame-Options',
             value: 'DENY',
           },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              'default-src https://vanilladefi.com http://vanilladefi.com https://plausible.io; img-src https://raw.githubusercontent.com https://ipfs.io https://assets.coingecko.com https://vanilladefi.com; connect-src ',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
         ],
       },
     ]
