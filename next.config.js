1d30a4444077bdd8d33d30d132ed8cdc99f72ca3
Jani Anttonen <jantto@utu.fi>
2021-04-12 14:34:52 +0300

    Add a referrer policy header

--- next.config.js
+++ next.config.js
@@ -42,6 +42,10 @@ module.exports = withMDX({
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
         ],
       },
     ]
