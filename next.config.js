8ad2a0bf69937eff5bd0fd71f72d6d6ccac036e0
Jani Anttonen <jantto@utu.fi>
2021-04-15 14:43:23 +0300

    Try to fix _next paths' headers

--- next.config.js
+++ next.config.js
@@ -13,6 +13,30 @@ const withMDX = require('@next/mdx')({
   },
 })
 
+const secureHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  /*  {
+    key: 'Content-Security-Policy',
+    value:
+      "default-src 'self'; img-src 'self' data: https://assets.coingecko.com https://ipfs.io https://raw.githubusercontent.com https://render.githubusercontent.com https://media.giphy.com; media-src 'self' https://media.giphy.com; script-src 'self' 'unsafe-inline' https://plausible.io; font-src 'self'; style-src 'self' 'unsafe-inline'; prefetch-src https://vanilladefi.com *equilibriumco.vercel.app; connect-src 'self' wss://bridge.walletconnect.org https://api.thegraph.com wss://api.thegraph.com https://eth-mainnet.alchemyapi.io wss://eth-mainnet.alchemyapi.io https://plausible.io; frame-ancestors 'none'; base-uri 'none'; form-action 'none'; manifest-src 'self';",
+  }, */
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+]
+
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   i18n: {
@@ -23,33 +47,16 @@ module.exports = withMDX({
   images: {
     domains: ['raw.githubusercontent.com', 'ipfs.io', 'assets.coingecko.com'],
   },
+  poweredByHeader: false,
   async headers() {
     return [
       {
         source: '/:path*{/}?',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          /*  {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src 'self'; img-src 'self' data: https://assets.coingecko.com https://ipfs.io https://raw.githubusercontent.com https://render.githubusercontent.com https://media.giphy.com; media-src 'self' https://media.giphy.com; script-src 'self' 'unsafe-inline' https://plausible.io; font-src 'self'; style-src 'self' 'unsafe-inline'; prefetch-src https://vanilladefi.com *equilibriumco.vercel.app; connect-src 'self' wss://bridge.walletconnect.org https://api.thegraph.com wss://api.thegraph.com https://eth-mainnet.alchemyapi.io wss://eth-mainnet.alchemyapi.io https://plausible.io; frame-ancestors 'none'; base-uri 'none'; form-action 'none'; manifest-src 'self';",
-          }, */
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
+        headers: secureHeaders,
+      },
+      {
+        source: '/_next',
+        headers: secureHeaders,
       },
     ]
   },
