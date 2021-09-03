3fc07fb1f7e20841a5a4eec45493267599027066
Jani Anttonen <jantto@utu.fi>
2021-09-03 13:52:00 +0300

    Expand user script to fetch legacy users

--- lib/vanilla/users.ts
+++ lib/vanilla/users.ts
@@ -1,27 +1,42 @@
 import { ethers } from 'ethers'
+import { getVanillaRouter } from 'hooks/useVanillaRouter'
 import { isAddress } from 'lib/tokens'
 import { VanillaVersion } from 'types/general'
-import { VanillaV1Router02__factory } from 'types/typechain/vanilla_v1.1/factories/VanillaV1Router02__factory'
-import { VanillaV1Router02 } from 'types/typechain/vanilla_v1.1/VanillaV1Router02'
-import { defaultProvider, getVanillaRouterAddress } from 'utils/config'
+import { defaultProvider } from 'utils/config'
 
 export const getUsers = async (): Promise<string[]> => {
-  const vnlRouter: VanillaV1Router02 = VanillaV1Router02__factory.connect(
-    getVanillaRouterAddress(VanillaVersion.V1_1),
-    defaultProvider,
-  )
+  const users: string[] = []
+
+  const vnlRouter = getVanillaRouter(VanillaVersion.V1_1, defaultProvider)
+  const vnlLegacyRouter = getVanillaRouter(VanillaVersion.V1_0, defaultProvider)
   const epoch = await vnlRouter.epoch()
+
+  // Fetch Vanilla v1.1 users
   const purchaseFilter: ethers.EventFilter = vnlRouter.filters.TokensPurchased()
   const events: ethers.Event[] = await vnlRouter.queryFilter(
     purchaseFilter,
     epoch.toNumber(),
   )
-  const users: string[] = []
   events.forEach((event) => {
     const userAddress = isAddress(event.args.buyer)
     if (userAddress && !users.includes(userAddress)) {
       users.push(userAddress)
     }
   })
+
+  // Fetch Vanilla v1.0 users
+  const legacyFilter: ethers.EventFilter =
+    vnlLegacyRouter.filters.TokensPurchased()
+  const legacyEvents: ethers.Event[] = await vnlLegacyRouter.queryFilter(
+    legacyFilter,
+    epoch.toNumber(),
+  )
+  legacyEvents.forEach((event) => {
+    const userAddress = isAddress(event.args.buyer)
+    if (userAddress && !users.includes(userAddress)) {
+      users.push(userAddress)
+    }
+  })
+
   return users
 }
