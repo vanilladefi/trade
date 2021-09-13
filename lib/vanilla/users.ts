9869d489ecce89638cf6fb350bfc793dfdfda5de
Jani Anttonen <jantto@utu.fi>
2021-09-13 14:55:58 +0300

    Update supply endpoint

--- lib/vanilla/users.ts
+++ lib/vanilla/users.ts
@@ -57,9 +57,7 @@ export const getBasicWalletDetails = async (
       ).balanceOf(walletAddress),
       vnlDecimals,
     )
-    console.log(vnlBalance)
     ethBalance = formatUnits(await getBalance(walletAddress, defaultProvider))
-    console.log(ethBalance)
   }
   return { vnlBalance, ethBalance }
 }
--- pages/api/[vanillaVersion]/vnl/supply.ts
+++ pages/api/[vanillaVersion]/vnl/supply.ts
@@ -1,19 +1,16 @@
 import { formatUnits } from 'ethers/lib/utils'
+import { getVanillaTokenContract } from 'hooks/useVanillaGovernanceToken'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { VanillaVersion } from 'types/general'
-import { ERC20 } from 'types/typechain/vanilla_v1.1/ERC20'
-import { ERC20__factory } from 'types/typechain/vanilla_v1.1/factories/ERC20__factory'
-import { defaultProvider, getVnlTokenAddress, vnlDecimals } from 'utils/config'
+import { parseVanillaVersionFromQuery } from 'utils/api'
+import { defaultProvider, vnlDecimals } from 'utils/config'
 
 export default async (
-  _req: NextApiRequest,
+  req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> => {
-  const vnl: ERC20 = ERC20__factory.connect(
-    getVnlTokenAddress(VanillaVersion.V1_1),
-    defaultProvider,
-  )
-  const totalSupply = await vnl.totalSupply()
+  const vnlVersion = parseVanillaVersionFromQuery(req.query)
+  const contract = getVanillaTokenContract(vnlVersion, defaultProvider)
+  const totalSupply = await contract.totalSupply()
   const formattedSupply = formatUnits(totalSupply, vnlDecimals)
   res.status(200).json(formattedSupply)
 }
