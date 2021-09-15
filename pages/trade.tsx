5430a8003a6a7c8099b421a6bf352e5b9e0a8c71
Jani Anttonen <jantto@utu.fi>
2021-09-15 14:52:23 +0300

    Try out Redis caching

--- pages/trade.tsx
+++ pages/trade.tsx
@@ -50,8 +50,10 @@ import {
   HandleBuyClick,
   HandleSellClick,
   Operation,
+  Token,
 } from 'types/trade'
 import { useWallet } from 'use-wallet'
+import { addToCache, getFromCache } from 'utils/cache'
 
 const TradeModal = dynamic(() => import('components/Trade/Modal'))
 
@@ -517,65 +519,82 @@ export const getStaticProps: GetStaticProps = async (): Promise<
 
   const currentBlockNumber = await getCurrentBlockNumber(UniswapVersion.v3)
 
+  const ethPrice = await getETHPrice(UniswapVersion.v3)
+
   // Fetch Uniswap V3 token info
-  let tokensV3 = getAllTokens(VanillaVersion.V1_1)
-  tokensV3 = await addLogoColor(tokensV3)
+  let tokensV3: Token[]
+  const tokensV3CacheKey = 'tokensV3'
+  const cachedTokensV3 = await getFromCache(tokensV3CacheKey)
+  if (cachedTokensV3) {
+    tokensV3 = JSON.parse(cachedTokensV3)
+  } else {
+    tokensV3 = getAllTokens(VanillaVersion.V1_1)
+    tokensV3 = await addLogoColor(tokensV3)
 
-  // Fetch these simultaneously
-  const [blocksPerHourV3, ethPrice] = await Promise.all([
-    getAverageBlockCountPerHour(),
-    getETHPrice(UniswapVersion.v3),
-  ])
+    // Fetch these simultaneously
+    const blocksPerHourV3 = await getAverageBlockCountPerHour()
 
-  if (ethPrice === 0 || currentBlockNumber === 0 || blocksPerHourV3 === 0) {
-    throw Error('Query failed')
-  }
+    if (ethPrice === 0 || currentBlockNumber === 0 || blocksPerHourV3 === 0) {
+      throw Error('Query failed')
+    }
 
-  tokensV3 = await addGraphInfo(UniswapVersion.v3, tokensV3, 0, ethPrice)
-  tokensV3 = addUSDPrice(tokensV3, ethPrice)
-  tokensV3 = tokensV3.map((token) => {
-    token.eligible = Eligibility.Eligible
-    return token
-  })
-  tokensV3 = await addObservationCardinality(tokensV3)
+    tokensV3 = await addGraphInfo(UniswapVersion.v3, tokensV3, 0, ethPrice)
+    tokensV3 = addUSDPrice(tokensV3, ethPrice)
+    tokensV3 = tokensV3.map((token) => {
+      token.eligible = Eligibility.Eligible
+      return token
+    })
+    tokensV3 = await addObservationCardinality(tokensV3)
 
-  block24hAgo = currentBlockNumber - 24 * blocksPerHourV3
+    block24hAgo = currentBlockNumber - 24 * blocksPerHourV3
 
-  // Add historical data (price change)
-  if (block24hAgo > 0) {
-    tokensV3 = await addGraphInfo(
-      UniswapVersion.v3,
-      tokensV3,
-      block24hAgo,
-      ethPrice,
-    )
+    // Add historical data (price change)
+    if (block24hAgo > 0) {
+      tokensV3 = await addGraphInfo(
+        UniswapVersion.v3,
+        tokensV3,
+        block24hAgo,
+        ethPrice,
+      )
+    }
+
+    await addToCache(tokensV3CacheKey, JSON.stringify(tokensV3))
   }
 
   // Fetch Uniswap V2 token info
-  let tokensV2 = getAllTokens(VanillaVersion.V1_0)
-  tokensV2 = await addLogoColor(tokensV2)
+  let tokensV2: Token[]
+  const tokensV2CacheKey = 'tokensV2'
+  const cachedTokensV2 = await getFromCache(tokensV2CacheKey)
+  if (cachedTokensV2) {
+    tokensV2 = JSON.parse(cachedTokensV2)
+  } else {
+    tokensV2 = getAllTokens(VanillaVersion.V1_0)
+    tokensV2 = await addLogoColor(tokensV2)
 
-  // Fetch these simultaneously
-  const blocksPerHourV2 = await getAverageBlockCountPerHour()
+    // Fetch these simultaneously
+    const blocksPerHourV2 = await getAverageBlockCountPerHour()
 
-  if (ethPrice === 0 || currentBlockNumber === 0 || blocksPerHourV2 === 0) {
-    throw Error('Query failed')
-  }
+    if (ethPrice === 0 || currentBlockNumber === 0 || blocksPerHourV2 === 0) {
+      throw Error('Query failed')
+    }
 
-  block24hAgo = currentBlockNumber - 24 * blocksPerHourV2
+    block24hAgo = currentBlockNumber - 24 * blocksPerHourV2
 
-  tokensV2 = await addGraphInfo(UniswapVersion.v2, tokensV2, 0, ethPrice)
-  tokensV2 = addUSDPrice(tokensV2, ethPrice)
-  tokensV2 = await addVnlEligibility(tokensV2, VanillaVersion.V1_0)
+    tokensV2 = await addGraphInfo(UniswapVersion.v2, tokensV2, 0, ethPrice)
+    tokensV2 = addUSDPrice(tokensV2, ethPrice)
+    tokensV2 = await addVnlEligibility(tokensV2, VanillaVersion.V1_0)
 
-  // Add historical data (price change)
-  if (block24hAgo > 0) {
-    tokensV2 = await addGraphInfo(
-      UniswapVersion.v2,
-      tokensV2,
-      block24hAgo,
-      ethPrice,
-    )
+    // Add historical data (price change)
+    if (block24hAgo > 0) {
+      tokensV2 = await addGraphInfo(
+        UniswapVersion.v2,
+        tokensV2,
+        block24hAgo,
+        ethPrice,
+      )
+    }
+
+    await addToCache(tokensV2CacheKey, JSON.stringify(tokensV2))
   }
 
   return {
---
+++ utils/cache.ts
@@ -0,0 +1,15 @@
+import Redis from 'ioredis'
+
+export const redis = new Redis(`rediss://${process.env.REDIS_URL}`, {
+  password: process.env.REDIS_PASSWORD,
+})
+
+export const addToCache = async (key: string, value: string): Promise<void> => {
+  await redis.set(key, value)
+  await redis.expire(key, 300)
+}
+
+export const getFromCache = async (key: string): Promise<string | false> => {
+  const value = await redis.get(key)
+  return value !== '' && value
+}
