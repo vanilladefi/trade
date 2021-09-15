b04a36587044aa318b50939f395d0669bebf8cf7
Jani Anttonen <jantto@utu.fi>
2021-09-15 16:06:07 +0300

    Add wallet balance cached query

--- utils/cache/meta.ts
+++ utils/cache/meta.ts
@@ -1,6 +1,7 @@
 import { getCurrentBlockNumber } from 'lib/block'
 import { getETHPrice } from 'lib/tokens'
-import { UniswapVersion } from 'types/general'
+import { getBasicWalletDetails } from 'lib/vanilla/users'
+import { UniswapVersion, VanillaVersion } from 'types/general'
 import { addToCache, getFromCache } from 'utils/cache'
 
 export const getBlockNumber = async (): Promise<number> => {
@@ -27,4 +28,30 @@ export const getEthPrice = async (): Promise<number> => {
     await addToCache(ethPriceCacheKey, ethPrice.toString())
   }
   return ethPrice
+}
+
+export const getWalletBalances = async (
+  walletAddress: string | false,
+): Promise<{ vnlBalance: string; ethBalance: string }> => {
+  let [vnlBalance, ethBalance] = ['0', '0']
+  if (walletAddress) {
+    const vnlBalanceCacheKey = `balance-vnl-${walletAddress}`
+    const ethBalanceCacheKey = `balance-eth-${walletAddress}`
+    const cachedVnlBalance = await getFromCache(vnlBalanceCacheKey)
+    const cachedEthBalance = await getFromCache(ethBalanceCacheKey)
+    if (cachedVnlBalance && cachedEthBalance) {
+      vnlBalance = cachedVnlBalance
+      ethBalance = cachedEthBalance
+    } else {
+      const response =
+        (walletAddress &&
+          (await getBasicWalletDetails(VanillaVersion.V1_1, walletAddress))) ||
+        undefined
+      vnlBalance = response.vnlBalance
+      ethBalance = response.ethBalance
+      await addToCache(vnlBalanceCacheKey, vnlBalance)
+      await addToCache(ethBalanceCacheKey, ethBalance)
+    }
+  }
+  return { vnlBalance, ethBalance }
 }
